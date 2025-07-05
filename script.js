3ea4d6a81952759865a6c40eb763f9f897f029e0
Lucia <luciasheel@gmail.com>
2025-07-05 16:33:35 -0700

    Add more features

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ resetBtn.addEventListener("click", function() {
     resetMessage.textContent = "";
   }, 2000);
 });
+
+let scrollToTopBtn = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+});
+
+scrollToTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
